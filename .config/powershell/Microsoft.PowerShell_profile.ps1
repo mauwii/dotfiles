d41601c903c3b1c42e391499daea3283c3897b9c
mauwii <Mauwii@outlook.de>
2023-07-23 00:43:08 +0200

    use profile.ps1

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,3 +0,0 @@
-$(/opt/homebrew/bin/brew shellenv pwsh) | Invoke-Expression # initialize brew env
-
-$(starship init powershell) | Invoke-Expression # initialize starship prompt
--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,1 +1,22 @@
-$(/opt/homebrew/bin/brew shellenv pwsh) | Invoke-Expression
+Function Test-CommandExists {
+    Param ($command)
+
+    $oldPreference = $ErrorActionPreference
+
+    $ErrorActionPreference = ‘stop’
+
+    try { if (Get-Command $command) { RETURN $true } }
+
+    Catch { Write-Debug “$command does not exist”; RETURN $false }
+
+    Finally { $ErrorActionPreference = $oldPreference }
+
+} #end function test-CommandExists
+
+If (Test-CommandExists /opt/homebrew/bin/brew) {
+    $(/opt/homebrew/bin/brew shellenv pwsh) | Invoke-Expression # initialize starship prompt
+}
+
+If (Test-CommandExists starship) {
+    $(starship init powershell) | Invoke-Expression # initialize starship prompt
+}
